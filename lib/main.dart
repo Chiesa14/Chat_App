7f65833ad615b41697ac67d37f3dad13a6f3bc5a
Chiesa14 <remychiesa14@gmail.com>
2023-11-22 21:30:59 +0200

    firebae login

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
-import 'package:chat_app/services/auth/login_or_register.dart';
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/services/auth/auth_gate.dart';
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ChangeNotifierProvider(
+    create: (context) => AuthService(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,9 +19,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRgister(),
+      home: AuthGate(),
     );
   }
 }
