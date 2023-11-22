c12852fd953946d73e09469ddd2de79c0645bfc5
Chiesa14 <remychiesa14@gmail.com>
2023-11-22 10:02:08 +0200

    toogle btn register and login

---
+++ lib/services/auth/login_or_register.dart
@@ -0,0 +1,29 @@
+import 'package:chat_app/pages/login_page.dart';
+import 'package:chat_app/pages/register_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginOrRgister extends StatefulWidget {
+  const LoginOrRgister({super.key});
+
+  @override
+  State<LoginOrRgister> createState() => _LoginOrRgisterState();
+}
+
+class _LoginOrRgisterState extends State<LoginOrRgister> {
+  bool showLoginPage = true;
+
+  void tooglePages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(onTap: tooglePages);
+    } else {
+      return RegisterPage(onTap: tooglePages);
+    }
+  }
+}
