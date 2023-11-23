603b0aa2c2102322a2b197a8b5683eb5f077422e
Chiesa14 <remychiesa14@gmail.com>
2023-11-23 10:39:23 +0200

    adding firestore db

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,8 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthService extends ChangeNotifier {
+  //firebase auth instance
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  //firebase store instance
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
 
 //log in
   Future<UserCredential> singInWithEmailAndPassword(
@@ -10,18 +15,27 @@ class AuthService extends ChangeNotifier {
     try {
       UserCredential userCredential = await _firebaseAuth
           .signInWithEmailAndPassword(email: email, password: password);
+      _fireStore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      }, SetOptions(merge: true));
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
     }
   }
 
-//create account
+  //create account
   Future<UserCredential> singUpWithEmailAndPassword(
       String email, password) async {
     try {
       UserCredential userCredential = await _firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password);
+      // create a new user document
+      _fireStore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      });
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
